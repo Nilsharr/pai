733ba1774866cf699a8d2ae5780352bfa818fa9b
Nilsharr <48214683+Nilsharr@users.noreply.github.com>
2022-11-29 01:45:16 +0100

    Remove unused import

--- pai6/src/main/java/com/example/controllers/UserController.java
+++ pai6/src/main/java/com/example/controllers/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
